cf0c4b32672d750b4b774b94bca4218ad437e0f4
Oskar S <oseparovic@gmail.com>
2022-09-21 13:25:26 -0700

    first working version of audio+video

--- index.js
+++ index.js
@@ -2,77 +2,175 @@ const ffmpeg = require("fluent-ffmpeg");
 const proc = new ffmpeg();
 const urlRegex = require("url-regex");
 const fetch = require("node-fetch");
+const download = require("node-hls-downloader").download;
+const hlsdl = require('@munirsafi/hls-dl');
+const fs = require('fs');
+const m3u8parser = require("mpd-m3u8-to-json").m3u8Parser;
+const hlsDownload = require('hls-download');
+const downloader = require('m3u8-multi-thread-downloader');
+const m3u8Downloader = require('m3u8_multi_downloader');
 
-var oneDownload = false;
 
-if (process.argv.length <= 2) {
-  console.log('Missing URL.');
-  console.log('Usage: node index.js <reddit-url> [output-folder]');
-  console.log('(the default output folder is ./');
-  console.log('Example: node index.js https://www.reddit.com/r/ItHadToBeBrazil/comments/chjfh1/i_wonder_how_much_that_upgrade_costed/ ~/Videos');
-  process.exit(0);
-}
-
-const url = process.argv[2];
-
-var outputFolder = './';
-
-if (process.argv.length >= 4) outputFolder = process.argv[3];
-
-if (!outputFolder.endsWith('/')) outputFolder += '/';
-
-const _res = [1080, 720, 480, 360, 240, 140, 120, '2_4_M', '1_2_M'];
-
-console.log(`Output folder > ${outputFolder}`);
-console.log(`URL > ${url}`);
-
-fetch(url)
-  .then(res => {
-    return res.text();
-  })
-  .then(body => {
-    const urls = body.match(urlRegex());
-    const mediaUrls = urls.filter(url => url.includes("v.redd.it"));
-    let mediaUrl = mediaUrls[0].split("https://v.redd.it/")[1];
-    mediaId = mediaUrl.split("/")[0];
-    testUrls(mediaId);
-
+async function mpdOption1(mpdUrl) {
+  let dest = 'mpdOption1.mp4';
+  const httpstream = hlsdl(mpdUrl, { headers: { 'Cache-Control': 'no-cache' } });
+  let file = fs.createWriteStream(dest);
+  httpstream.pipe(file);
+  file.on('finish', function () {
+    file.close();
   });
-
-function testUrls(mediaId) {
-  console.log('Hold on, Fetching the Best Quality');
-  _res.forEach(res => {
-    fetch(`https://v.redd.it/${mediaId}/DASH_${res}`)
-      .then(response => {
-        if (response.status === 200 && !oneDownload) {
-          oneDownload = true;
-          console.log('Downloading With : ' + res + ' Please Wait ...');
-          scrape(mediaId, res);
-        }
-      });
+  httpstream.on('error', (err) => { // Handle errors
+    fs.unlink(dest, () => console.log(err.message)); // delete the (partial) file and then return the error
+  });
+  httpstream.on('status', info => {
+    // do something with info
+    console.log(info);
   });
 }
 
+async function hlsOption1(hlsUrl) {
+  let res = await fetch(hlsUrl);
+  let body = await res.text();
+  let json = m3u8parser(body, hlsUrl);
+  console.log(json);
+  let level = json.levels[json.levels.length - 1].url;
+  console.log("==============");
+  console.log("using level: " + level);
 
-function scrape(mediaId, res) {
-  proc.addInput(`https://v.redd.it/${mediaId}/DASH_${res}`)
-    .output(`${outputFolder}${mediaId}-${res}.mp4`)
-    .on("error", err => {
-      console.log("Error: " + err);
-    })
-    .on("end", () => {
-      console.log("Done");
-    });
+  let res2 = await fetch(level);
+  let body2 = await res2.text();
+  let json2 = m3u8parser(body2, hlsUrl);
+  console.log(json2);
+  await new hlsDownload({ m3u8json: json2, output: "./hlsOption1.mp4", baseurl: hlsUrl }).download();
+}
 
-  fetch(`https://v.redd.it/${mediaId}/DASH_audio.mp4`)
-    .then(resp => {
-      if (resp.status === 200) {
-        console.log('Founded audio track...')
-        proc.addInput(`https://v.redd.it/${mediaId}/audio`);
-      } else {
-        console.log('No audio track...');
+async function hlsOption2(hlsUrl) {
+  await download({
+    quality: "best",
+    concurrency: 5,
+    outputFile: "hlsOption2.mp4",
+    streamUrl: hlsUrl,
+  });
+}
+
+async function hlsOption4(hlsUrl) {
+  let res = await fetch(hlsUrl);
+  let body = await res.text();
+  let json = m3u8parser(body, hlsUrl);
+  console.log(json);
+  let level = json.levels[json.levels.length - 1].url;
+  console.log("using stream: " + level);
+  downloader.download({
+    url: level,
+    processNum: 4, // 同时开启的线程数,线程不宜开的过多，否则容易造成资源无法正常下载的情况
+    filePath: './', // 所存放的文件夹
+    fileName: 'hlsOption4.mp4' // 视频资源的文件名
+  });
+}
+
+async function hlsOption5(hlsUrl) {
+  let res = await fetch(hlsUrl);
+  let body = await res.text();
+  let json = m3u8parser(body, hlsUrl);
+  console.log(json);
+  let level = json.levels[json.levels.length - 1].url;
+  console.log("using stream: " + level);
+  m3u8Downloader.download({
+    url: level,
+    processNum: 15,
+    filePath: './',
+    filmName: 'hlsOption5.mp4'
+  });
+}
+
+async function hlsOption6(hlsUrl) {
+  //https://video.stackexchange.com/questions/10730/combine-video-and-audio-ts-segments-coming-from-hls-stream
+  let res = await fetch(hlsUrl);
+  let body = await res.text();
+  let json = m3u8parser(body, hlsUrl);
+  console.log(json);
+  let levels = json.levels;
+  let medias = json.medias;
+  let video = null;
+  let audio = null;
+  // fetch best level
+  if (levels != null && levels.length > 0) {
+    let bestBandwidth = 0;
+    for (let level of levels) {
+      if (parseInt(level.bandwidth) > bestBandwidth) {
+        bestBandwidth = parseInt(level.bandwidth);
+        video = level.url;
       }
-      console.log('Downloading and converting...');
-      proc.run();
-    });
+    }
+  }
+  // fetch best audio
+  if (medias != null && medias.length > 0) audio = medias[medias.length - 1].url;
+  infs = new ffmpeg();
+  if (audio != null) infs.addInput(audio)
+  if (video != null) infs.addInput(video)
+  infs.outputOptions([
+    '-async 1'
+  ])
+  infs.output('test.mp4')
+  infs.on('start', function (commandLine) {
+    console.log('Spawned Ffmpeg with command: ' + commandLine);
+  })
+  infs.on('error', function (err, stdout, stderr) {
+    console.log('An error occurred: ' + err.message, err, stderr);
+  })
+  infs.on('progress', function (progress) {
+    console.log('Processing: ' + progress.percent + '% done')
+  })
+  infs.on('end', function (err, stdout, stderr) {
+    console.log('Finished processing!' /*, err, stdout, stderr*/)
+  })
+  infs.run();
 }
+
+async function main() {
+  if (process.argv.length <= 2) {
+    console.log('Missing URL.');
+    console.log('Usage: node index.js <reddit-url> [output-folder]');
+    console.log('(the default output folder is ./');
+    console.log('Example: node index.js https://www.reddit.com/r/ItHadToBeBrazil/comments/chjfh1/i_wonder_how_much_that_upgrade_costed/ ~/Videos');
+    process.exit(0);
+  }
+
+  const url = process.argv[2];
+
+  var outputFolder = './';
+
+  if (process.argv.length >= 4) outputFolder = process.argv[3];
+
+  if (!outputFolder.endsWith('/')) outputFolder += '/';
+
+  console.log(`Output folder > ${outputFolder}`);
+  console.log(`URL > ${url}`);
+
+  let res = await fetch(url);
+  let json = await res.json();
+  if (json.data == null) json = json[0]; //api has no children at root level, but posturl.json does
+  const redditVideoNode = json.data.children[0].data.media.reddit_video;
+  const hlsUrl = redditVideoNode.hls_url;
+  const dashUrl = redditVideoNode.dash_url;
+  // const hlsUrl = "https://cph-p2p-msl.akamaized.net/hls/live/2000341/test/master.m3u8";
+  // const hlsUrl = "https://yun.kubozy-youku-163.com/20190709/16666_5a9c65b6/1000k/hls/index.m3u8";
+
+  // not working
+  // await hlsOption1(hlsUrl);
+  // await mpdOption1(dashUrl);
+
+  // no audio
+  // await hlsOption2(hlsUrl);
+  // await hlsOption4(hlsUrl);
+  // await hlsOption5(hlsUrl);
+
+  // fully working
+  await hlsOption6(hlsUrl);
+}
+
+
+// testing only
+// main();
+
+exports.hlsDownload = hlsOption6;
